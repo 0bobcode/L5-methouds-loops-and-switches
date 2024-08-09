e369a4b51fa38169503eb68340bfc1e0b596a7ff
bobchopra <bobchopracoding@gmail.com>
2024-08-09 09:49:08 -0400

    add string methods

---
+++ script.js
@@ -0,0 +1,5 @@
+let char = "bob choPra";
+
+console.log(`using the method .toUpperCase() ${char.toUpperCase()} `);
+console.log(`using the method .toLowerCase() ${char.toLowerCase()}`)
+console.log(`using the method .starsWith() ${char.startsWith("b")}`);
